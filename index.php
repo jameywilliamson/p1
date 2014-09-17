1bb9bbf41ba13bdd6ca8c500e56fd97f50487ed5
Live <jameywilliamson@g.harvard.edu>
2014-09-17 00:19:10 -0400

    Added readme file

---
+++ index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+<!--Header for web page--> 
+<head>
+	<title>Jamey Williamson DWA15 Project</title>
+
+	<!--Defining Div id's-->	
+	<style>
+
+		
+		#header {
+		text-align:center;
+		margin:auto;
+		font-size:250%;
+		
+		}
+		
+		#center {
+		text-align:center;
+		}
+		
+		img {
+			display:block;
+			margin:auto;
+			width:30%;
+			}
+		
+		.links {
+			text-align:center;
+			margin:auto;
+			width:30%;
+			font-size:150%;
+			}
+			
+		.body {
+		background-color: white;
+		width: 80%;
+		margin: 0 auto;
+		font-size:40px;
+			}
+			
+	</style>
+</head>
+
+<!--Setting background color-->
+<style>
+body
+{ background-color: #80F800;
+}
+</style>
+
+<body>
+
+<div id="header">
+<h1>Jamey Williamson<br>Dynamic Web Applications</h1>
+</div>
+
+<div id="center">
+<img src="images/jamey.jpg" alt="Jamey and his grandmother">
+</div>
+
+<div class="links">
+	<h1>Project Links</h1>
+	<h2>P2</h2>
+		<b>Digital Ocean: (Future Link) <br>
+		Github: (Future Link)</b>
+		
+	<h2>P3</h2>
+		<b>Digital Ocean: (Future Link) <br>
+		Github: (Future Link)</b>
+		
+	<h2>P4</h2>
+		<b>Digital Ocean: (Future Link) <br>
+		Github: (Future Link)</b>
+</div>
+
+<div class="body">
+
+<p>Hey everyone! My name is Jamey and I am from North Carolina.I am an IT professional working 
+for TIAA-CREF as a Project Coordinator. I've worked for companies such as Intel, Lockheed Martin,
+T-Mobile, and the University of North Carolina Agricultural & Technical College. I've worked mainly
+as a troubleshooter to computer and network issues.
+</p>
+
+<p>My purpose for taking this class is to have a better understanding of web-based applications and
+to gain the skills and resources required to perhaps start the next phase of my career as a web developer.
+I love to improve myself and I believe taking the plunge into this new arena will open up more exciting 
+options in my future.
+</p>
+
+<p>I learned programming languages such as C++ and Linux mainly. I have basic HTML knowledge (which you
+can probably tell if you are looking at my code on Github...) and mainly create batch files to make my life 
+easier at work. 
+</p>
+
+<p>I'm a PC user, I've used every Windows OS from 95 to XP to Windows 8. I have used and ran virtual consoles in
+the past for Linux and Mac OSX. 
+</p>
+
+<p>The main thing to know about me is I will do everything I can, as long as I can until I tackle a problem I'm facing...
+including creating this project. I've spent most of my adulthood travelling to every major as part of my time with Lockheed Martin.
+I've travelled all over the U.S., seen some of the best locations, and decided to enrol at Harvard University after taking a tour while I was working
+in Boston. 
+</p>
+</div>
+
+</body>
+</html>
